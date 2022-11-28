139cf048fc48cb3b45cb80fcbfe3adb3f7fbaaad
Raphaël Velt <raphv@users.noreply.github.com>
2022-11-28 12:35:07 +0100

    App + new data

---
+++ app/pre-build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp index.html app/www
+cp css/emojipaint.css app/www/css/emojipaint.css
+cp data/emojidata.json app/www/data/emojidata.json
+cp js/emojipaint.js app/www/js/emojipaint.js
